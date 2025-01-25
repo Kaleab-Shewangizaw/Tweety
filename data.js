e5753eef75f65facef31cec2ab3d1e6cd2fb4774
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-01-25 06:48:57 +0300

    prepared demo data

---
+++ data.js
@@ -0,0 +1,83 @@
+let data = [
+  {
+    handle: "Twimba",
+    pfp: "images/pp2.jpg",
+    content: "this is the contnet of my post",
+    isLiked: false,
+    isRetweeted: false,
+    likes: 11,
+    retweets: 10,
+    shares: 50,
+    replies: [
+      {
+        handle: "Elon Musk",
+        pfp: "images/pp3.png",
+        content: "I agree with you, Twimba!",
+      },
+      {
+        handle: "Kaleab",
+        pfp: "images/pp1.jpg",
+        content: "Can't agree more!"
+      }
+    ]
+  },
+  {
+    handle: "Kaleab",
+    pfp: "images/pp2.jpg",
+    content: "this is the contnet of my post",
+    isLiked: false,
+    isRetweeted: false,
+    likes: 110,
+    retweets: 100,
+    shares: 99,
+    replies: [
+      {
+        handle: "Elon Musk",
+        pfp: "images/pp3.png",
+        content: "I agree with you, Twimba!",
+      },
+      {
+        handle: "Kaleab",
+        pfp: "images/pp1.jpg",
+        content: "Can't agree more!"
+      },
+      {
+        handle: "Kaleab",
+        pfp: "images/pp1.jpg",
+        content: "Can't agree more!"
+      }
+    ]
+  },
+  {
+    handle: "Someone",
+    pfp: "images/pp2.jpg",
+    content: "this is the contnet of my post",
+    isLiked: false,
+    isRetweeted: false,
+    likes: 123,
+    retweets: 110,
+    shares: 90,
+    replies: []
+  },
+
+  {
+    handle: "Elon Musk",
+    pfp: "images/pp3.jpg",
+    content: "this is the contnet of my post",
+    isLiked: false,
+    isRetweeted: false,
+    likes: 270,
+    retweets: 199,
+    shares: 110,
+    replies: [
+      {
+        handle: "Kaleab Shewangizaw",
+        pfp: "images/pp3.png",
+        content: "I agree with you, Twimba!",
+      },
+    ]
+  },
+]
+
+
+export default data
